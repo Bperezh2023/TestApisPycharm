c00502af4f7c13aea065fa31d3a91736e1953738
jordanchp <jchampet@bi.com.gt>
2024-02-26 17:10:33 -0600

    nueva ruta de controlaodres

--- api_rest/Views/r_view.py
+++ api_rest/Views/r_view.py
@@ -1,14 +1,14 @@
 
 
 from flask import Blueprint, jsonify, request
-from Controller.r_control import consulta_grupos
-from Controller.r_control_2 import consulta_rest_mesa
+from Controller.tickets.grupos.r_control import consulta_grupos
+from Controller.tickets.usuarios.r_control_u import consulta_rest_mesa
 
 rest_blueprint = Blueprint('rest', __name__)
 
 @rest_blueprint.route('/consulta-grupos', methods=['POST'])
 def consultaGrupos():
-    print(request.data.decode('utf-8'))
+    # print(request.data.decode('utf-8'))
     data = consulta_grupos(request.data.decode('utf-8'))
     response = jsonify(data)
     print(response)
@@ -18,7 +18,7 @@ def consultaGrupos():
 @rest_blueprint.route('/consulta-usuarios', methods=['POST'])
 def consultaUsuarios():
     ##consulta_rest_mesa
-    print(request.data.decode('utf-8'))
+    # print(request.data.decode('utf-8'))
     data = consulta_rest_mesa(request.data.decode('utf-8'))
     response = jsonify(data)
     print(response)
