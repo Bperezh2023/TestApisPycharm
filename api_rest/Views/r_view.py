e751845059a226116fc1a11248a47120ca2e2faa
jordanchp <jchampet@bi.com.gt>
2024-02-26 18:10:22 -0600

    cambios en print de datos

--- api_rest/Views/r_view.py
+++ api_rest/Views/r_view.py
@@ -8,7 +8,7 @@ rest_blueprint = Blueprint('rest', __name__)
 
 @rest_blueprint.route('/consulta-grupos', methods=['POST'])
 def consultaGrupos():
-    # print(request.data.decode('utf-8'))
+    print(request.data.decode('utf-8'))
     result = consulta_grupos(request.data.decode('utf-8'))
     return result
 
@@ -16,6 +16,6 @@ def consultaGrupos():
 @rest_blueprint.route('/consulta-usuarios', methods=['POST'])
 def consultaUsuarios():
     ##consulta_rest_mesa
-    # print(request.data.decode('utf-8'))
+    print(request.data.decode('utf-8't))
     result = consulta_rest_mesa(request.data.decode('utf-8'))
     return result
