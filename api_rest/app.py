dd27f4280b704b93138dc7850b6a7a8a79b603bb
jordanchp <jchampet@bi.com.gt>
2024-02-27 09:29:45 -0600

    subida de cambios finales

--- api_rest/app.py
+++ api_rest/app.py
@@ -7,4 +7,4 @@ app.register_blueprint(rest_blueprint)
 
 if __name__ == '__main__':
     ##192.168.176.86 / 172.17.194.115
-    app.run(host='172.17.192.124', port=5000, debug=True)
+    app.run(host='172.17.194.83', port=5000, debug=True)
